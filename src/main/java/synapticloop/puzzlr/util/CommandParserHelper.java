5e744c2eb4371f0aa9af8515a15986c534cccaee
synapticloop <osmanj@synapticloop.com>
2021-05-04 13:41:45 +1000

    added in copyright

--- src/main/java/synapticloop/puzzlr/util/CommandParserHelper.java
+++ src/main/java/synapticloop/puzzlr/util/CommandParserHelper.java
@@ -1,5 +1,21 @@
 package synapticloop.puzzlr.util;
 
+/*
+ * Copyright (c) 2021 Synapticloop.
+ * 
+ * All rights reserved.
+ * 
+ * This code may contain contributions from other parties which, where 
+ * applicable, will be listed in the default build file for the project 
+ * ~and/or~ in a file named CONTRIBUTORS.txt in the root of the project.
+ * 
+ * This source code and any derived binaries are covered by the terms and 
+ * conditions of the Licence agreement ("the Licence").  You may not use this 
+ * source code or any derived binaries except in compliance with the Licence.  
+ * A copy of the Licence is available in the file named LICENSE.txt shipped with 
+ * this source code or binaries.
+ */
+
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -85,7 +101,7 @@ public class CommandParserHelper {
 			System.exit(-1);
 		}
 	}
-	
+
 	private void logCommandLine(String param, String value) {
 		LOGGER.info(PARSED_AND_CALCULATED_PARAMETER, param, value);
 	}
